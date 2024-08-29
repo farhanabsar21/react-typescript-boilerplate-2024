1fca174158d1b8321c3ddaa8d12f5988ed8124e5
farhan21 <farhanabsar21@gmail.com>
2024-08-29 22:07:28 +0200

    tailwind added

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}', // Adjust the paths according to your project structure
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'postcss-loader', // Ensure this loader is used
+        ],
+      },
+    ],
+  },
+};
